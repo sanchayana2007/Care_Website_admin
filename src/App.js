5bed7d23e49d9a4ff128742991d71b543a8adad7
Sanch <sanchayan@xlayer.in>
2024-04-18 10:02:20 +0530

    added commits

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const url = "https://api.restful-api.dev/objects";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
+      <center>
+        {data.map((dataObj, index) => {
+          return (
+            <div
+              style={{
+                width: "15em",
+                backgroundColor: "#35D841",
+                padding: 2,
+                borderRadius: 10,
+                marginBlock: 10,
+              }}
+            >
+              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
+            </div>
+          );
+        })}
+      </center>
+    </div>
+  );
+}
+
+export default App; 
